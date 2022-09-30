48a8e2324f57e4dec7f3cf4a9f81b1950870cc16
gs-cnam <gschmidt@cfai-formation.fr>
2022-09-30 23:34:22 +0200

    efef

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + "/dist/<tp02-schmidt-guillaume>"));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/<tp02-schmidt-guillaume>/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
