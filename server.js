bbb5f1bec1553010ece734e9e1b0b9b72ea5fe28
gs-cnam <gschmidt@cfai-formation.fr>
2022-09-30 23:38:17 +0200

    ergerg

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<tp02-schmidt-guillaume>"));
+app.use(express.static(__dirname + "/dist/tp02-schmidt-guillaume"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<tp02-schmidt-guillaume>/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/tp02-schmidt-guillaume/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
